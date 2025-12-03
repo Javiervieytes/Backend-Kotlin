702f35b2eb1c874aec74567696fe048063c5e69a
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-12-02 22:26:19 -0300

    Delete demo directory

--- demo/src/main/java/com/kotlin/demo/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kotlin.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kotlin.demo.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
